93db85795d1a3e1a5ad41d7deed77de79b482910
unknown <aitorparadalinaje002@gmail.com>
2023-02-13 17:36:06 +0100

    Update move character in an array bidimensional

--- js/game.js
+++ js/game.js
@@ -1,49 +1,47 @@
 'use strict';
 
 var arrayTablero = [];
-var posX = 0;
-var posY = 0;
+var jugador = 0;
 var posTotales = 99;
+var oldx=0;
+var oldy = 0;
 window.onload = () =>{
-    arrayTablero = inicializarTablero(10,10);
+    inicializar();
     dibujarTablero();
-    let posicionPersonaje = getPosicionPersonaje(posX,posY);
-    movimientoPersonaje(posX,posY);
-}
-
-function inicializarTablero(nFilas,nColumnas){
-
-    for (let i = 0; i < nFilas;i++) {
-        arrayTablero[i]= [];
-        for (let j = 0; j < nColumnas;j++) {
-           arrayTablero[i][j] = 0;
-        }   
-    }
-    //1 Heroe 
-    //0 Casillas restantes
-    arrayTablero[posX][posY] = 1;
-    return arrayTablero;
+    movimientoPersonaje();
+    generarBoton();
 }
 
 function dibujarTablero(){
-    const container = document.querySelector(".container");
+    const tablero = document.querySelector(".tablero");
+    while (tablero.firstChild) {
+        tablero.removeChild(tablero.firstChild);
+      }
+    let divTablero = document.createElement("div");
+    divTablero.classList.add("tablero");
+    tablero.appendChild(divTablero);
+   // arrayTablero[oldx][oldy].classList.add("hero");
+    let cont = 0;
     for (let i = 0; i < 10;i++) {
         for (let j = 0; j < 10;j++) {
-            let casilla = document.createElement("div");
+           let casilla = document.createElement("div");
             if(arrayTablero[i][j] == 1){
                 casilla.classList.add("personaje","casilla");
             }else{
                 casilla.classList.add("suelo","casilla");
             }
-            container.appendChild(casilla);
+            
+            casilla.setAttribute("numero",cont++);
+            divTablero.appendChild(casilla);
         } 
         let divSalto = document.createElement("div");
         divSalto.classList.add("salto");
-        container.appendChild(divSalto);  
+        divTablero.appendChild(divSalto);
+         
     }
 }
 
-function movimientoPersonaje(posX,posY){
+function movimientoPersonaje(){
 
     var divsPersonaje = document.getElementsByClassName("personaje");
     var divs = document.querySelectorAll(".casilla");
@@ -51,61 +49,103 @@ function movimientoPersonaje(posX,posY){
         var divPersonaje = divsPersonaje[i];
         divPersonaje.addEventListener("click",function(e){
         console.log(divPersonaje);
-        posX = posX+2;
-        
-        if(posX > 9){
-            posY=1;
+        if(jugador==0){
+            jugador += 4;
         }
+        console.log("Jugador "+jugador);
 
-        if(posX > 19){
-            posY=2;
-        }
-
-        if(posX > 29){
-            posY=3;
-        }
-
-        if(posX > 39){
-            posY=4;
-        }
-
-        if(posX > 59){
-            posY=5;
-        }
-
-        if(posX > 69){
-            posY=6;
-        }
-
-        if(posX > 79){
-            posY=7;
-        }
-
-        if(posX > 89){
-            posY=8;
-        }
-
-        if(posX >= 99){
-            posY=9;
-        }
-
-        divPersonaje.classList.remove("personaje");
-        let hasPersonajeClass = divPersonaje.classList.contains("personaje");
-        if(!hasPersonajeClass){
-            console.log("No tiene la clase personaje");
-            divPersonaje.removeEventListener("click",divPersonaje,false);
-            let newdiv = divs[posX];
-            newdiv.style.backgroundColor="blue";
-            console.log(posX);
-            console.log(posY);
-            divPersonaje.style.backgroundColor="black";
-        }
+        let numeroAleatorio = getNumeroAleatorio();
+        console.log("Numero Aleatorio : "+numeroAleatorio);
 
        })
     }
 }
 
+function generarBoton(){
+    let divDado = document.createElement("div");
+    divDado.classList.add("dado");
+    let divBoton = document.createElement("div");
+    divBoton.classList.add("boton");
+    var container = document.querySelector(".container");
+    let boton = document.createElement("button");
+    boton.textContent="Tirar Dado";
+    var imagen = document.createElement("img");
+    if(imagen.src=""){
+        imagen.src="../images/dado1.jpg";
+    }
+    container.appendChild(divBoton);
+    divBoton.appendChild(boton);
+    boton.addEventListener("click",function(e){
+        let numero = 0;
+        numero = getNumeroAleatorio();
+        switch(numero){
+            case 1:
+                imagen.src="../images/dado1.jpg";
+                break;
+            case 2:
+                imagen.src="../images/dado2.jpg";
+                break;
+            case 3:
+                imagen.src="../images/dado3.jpg";
+                break;
+            case 4:
+                imagen.src="../images/dado4.jpg";
+                break;
+            case 5:
+                imagen.src="../images/dado2.jpg";
+                break;
+            case 6:
+                imagen.src="../images/dado6.jpg";
+                break;
+            default:
+        }
+        getPosicionPersonaje(numero);
+        dibujarTablero();
+    });
+    
+    container.appendChild(divDado);
+    divDado.appendChild(imagen);
 
-function getPosicionPersonaje(posX,posY){
-    return arrayTablero[posX][posY];
+}
+
+function getNumeroAleatorio(){
+    return Math.floor(Math.random() * (6 - 1) + 1);
+}
+
+
+function getPosicionPersonaje(avance){
+    jugador = jugador + avance;
+    let x = jugador%10-1;
+    let y = parseInt(jugador/10)-1;
+    arrayTablero[y][x] = 1;
+    arrayTablero[oldy][oldx] = 0;
+    oldx = x;
+    oldy = y;
+   // alert("Te has movido "+avance+" casillas");
+   console.log("Casilla X Nueva : "+x);
+   console.log("Casilla Y Nueva : "+y);
+   console.log("Casilla X Old : "+oldx);
+   console.log("Casilla Y Old : "+oldy);
+
+   if(x==9 & y == 9){
+    alert("Has ganado");
+   }else{
+    let diferencia = 99 -avance;
+    console.log(diferencia);
+   }
+}
+
+function inicializar(){
+
+     for (let i = 0; i < 10;i++) {
+        arrayTablero[i] = [];
+        for (let j = 0; j < 10;j++) {
+            arrayTablero[i][j] = 0;
+        }
+    }
+    arrayTablero[0][0] = 1;
+    let divTablero = document.createElement("div");
+    divTablero.classList.add("tablero");
+    let container = document.querySelector(".container");
+    container.appendChild(divTablero);
 }
